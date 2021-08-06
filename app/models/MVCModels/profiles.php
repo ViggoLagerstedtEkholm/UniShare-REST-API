66444250fb7b02f6878e8080fa7a16af3674d3cc
Viggo <viggo.lagerstedtekholm@gmail.com>
2021-08-07 00:05:54 +0200

    Added profiles model.

---
+++ app/models/MVCModels/profiles.php
@@ -0,0 +1,71 @@
+<?php
+namespace App\Models\MVCModels;
+
+require_once realpath($_SERVER['DOCUMENT_ROOT'] . "/UniShare/vendor/autoload.php");
+
+use App\Models;
+use App\Models\MVCModels\Database;
+
+class Profiles extends Database{
+  function uploadImage($image, $ID){
+    $sql = "UPDATE users SET userImage =? WHERE usersID = ?;";
+    $stmt = mysqli_stmt_init($this->getConnection());
+
+    if(!mysqli_stmt_prepare($stmt, $sql)){
+      header("location: ../../views/profile.php?ID=$ID&error=uploadfail");
+      exit();
+    }
+
+    mysqli_stmt_bind_param($stmt, "si", $image, $ID);
+    mysqli_stmt_execute($stmt);
+    if (!mysqli_stmt_execute($stmt))
+    {
+        echo "Failed to insert image! " . $stmt->error;
+    }
+    mysqli_stmt_close($stmt);
+
+    header("location: ../../views/profile.php?ID=$ID");
+    exit();
+  }
+
+  function addVisitor($ID, $user){
+    $visits = $user["visits"];
+
+    $updatedVisits = $visits + 1;
+
+    $sql = "UPDATE users SET visits =? WHERE usersID = ?;";
+
+    $stmt = mysqli_stmt_init($this->getConnection());
+
+    if(!mysqli_stmt_prepare($stmt, $sql)){
+      header("location: ../profile.php?error=visitcounterror");
+      exit();
+    }
+
+    mysqli_stmt_bind_param($stmt, "ii", $updatedVisits , $ID);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+
+    return $updatedVisits;
+  }
+
+  function addVisitDate($ID){
+    $sql = "UPDATE users SET lastOnline =? WHERE usersID = ?;";
+
+    date_default_timezone_set("Europe/Stockholm");
+    $Date = date("Y-m-d",time());
+
+    $stmt = mysqli_stmt_init($this->getConnection());
+
+    if(!mysqli_stmt_prepare($stmt, $sql)){
+      header("location: ../views/profile.php?error=dateinsertfail");
+      exit();
+    }
+
+    mysqli_stmt_bind_param($stmt, "si", $Date, $ID);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+
+    return $Date;
+  }
+}
